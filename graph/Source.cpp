b55a30bc69f2a57a203a166a3140a27488f58407
Maximilian Hallqvist <maximilian.hallqvist@gmail.com>
2023-09-15 15:31:10 +0100

    DET FUCKING FUNKAR

--- graph/Source.cpp
+++ graph/Source.cpp
@@ -1,69 +1,144 @@
 #include <iostream>
+#include <vector>
+#include <map>
 #include <string>
 #include <cmath>
+
 using namespace std;
 
-int main() {
-	constexpr int range = 40;
-	constexpr int rows = range + 1;
-	constexpr int cols = range + 1;
+void gen_graph(string**& graph, int range, vector<map<string, double>>& p) {
+	int rows = range + 1;
+	int cols = range + 1;
 
 	int half_v = range / 2;
-	int graph[rows][cols];
-
 	int range_x = 0;
 	int range_y = 0;
 
-	string color_x;
-	string color_y;
+	/*
+	Graph is a pointer that points to an array of pointers,
+	and each of these pointers in turn points to a row of strings,
+	forming a 2D array.This is a common technique for dynamically allocating a
+	2D array where the size of the array is determined at runtime.
+	*/
 
+	// Dynamically allocate memory for the graph array
+	graph = new string * [rows];
+	for (int i = 0; i < rows; i++) {
+		graph[i] = new string[cols];
+	}
+
+	string color_x = "\033[31m";
+	string color_y = "\033[31m";
+	string color_g = "\033[32m";
+	string c_end = "\033[0m";
+
+	// Generate graph
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
-			// middle vertical row
+			// Middle vertical row
 			if (j == half_v) {
-				graph[i][j] = range_y;
+				graph[i][j] = color_y + to_string(range_y) + c_end;
 
-				if (i < half_v) // while on the posetive side of 0
+				if (i < half_v) // While on the positive side of 0
 					range_y--;
-				else // while on the negative side of 0
+				else // While on the negative side of 0
 					range_y++;
 
-				// switch at center
-				if (range_y < 0 and i < half_v) // count 0 9 8 7 ...
+				// Switch at center
+				if (range_y < 0 and i < half_v) // Count 0 9 8 7 ...
 					range_y = 9;
-				else if (range_y > 9) // count 0 1 2 3 ...
+				else if (range_y > 9) // Count 0 1 2 3 ...
 					range_y = 0;
 
-				// hotfix
-				if (i == half_v) // move range_x once at center
+				// Hotfix
+				if (i == half_v) // Move range_x once at center
 					range_x++;
 			}
-
-			// middle horizontal row
+			// Middle horizontal row
 			else if (i == half_v) {
-				graph[i][j] = range_x;
+				graph[i][j] = color_x + to_string(range_x) + c_end;
 
-				if (j < half_v) // while on the negative side of 0
+				if (j < half_v) // While on the negative side of 0
 					range_x--;
-				else // while on the posetive side of 0
+				else // While on the positive side of 0
 					range_x++;
 
-				// switch at center
-				if (range_x < 0 and j < half_v) // count 0 9 8 7 ...
+				// Switch at center
+				if (range_x < 0 and j < half_v) // Count 0 9 8 7 ...
 					range_x = 9;
-				else if (range_x > 9) // count 0 1 2 3 ...
+				else if (range_x > 9) // Count 0 1 2 3 ...
 					range_x = 0;
 			}
-
 			else {
-				graph[i][j] = 0;
+				graph[i][j] = to_string(0);
 			}
 
-			// display graph
+			for (auto& point : p) {
+				if (i == point["y"] + range / 2 and j == point["x"] + range / 2) {
+					graph[i][j] = color_g + graph[i][j] + c_end;
+				}
+			}
+		}
+	}
+}
+
+void dp_graph(string**& graph, int range) {
+	// Display graph
+	for (int i = 0; i <= range; i++) {
+		for (int j = 0; j <= range; j++) {
 			cout << graph[i][j] << " ";
 		}
 		cout << endl;
 	}
+}
+
+vector<double> sec_deg_f(double a, double b, double c) {
+	// x = (-b +- sqrt(b^2 - 4ac)) / (2a)
+	double v_dif = sqrt(pow(b, 2) - 4 * a * c); // +-
+
+	// ansure v_dif is a real number
+	if (!isfinite(v_dif)) {
+		cout << "The equation has no real roots" << endl;
+	}
+
+	double v1 = (-b + v_dif) / (2 * a);
+	double v2 = (-b - v_dif) / (2 * a);
+
+	vector<double> x{ v1, v2 };
+	return x;
+}
+
+void first_deg_f(double k, double m, int range, vector<map<string, double>>& p) {
+	for (int x = -range / 2; x <= range / 2; x++) {
+		map<string, double> point;
+		point["x"] = x;
+		point["y"] = k * x + m;
+		p.push_back(point);
+	}
+}
+
+int main() {
+	const int range = 40;
+	string** graph = nullptr;
+
+	vector<map<string, double>> p; // måste specifiera hur många objekt på vectorn???
+	int b = 2;
+	int c = 3;
+
+	first_deg_f(b, c, range, p);
+
+	gen_graph(graph, range, p);
+
+	dp_graph(graph, range);
+
+	/*
+	// Deallocate dynamically allocated memory
+	for (int i = 0; i <= range; i++) {
+		delete[] graph[i];
+	}
+	delete[] graph;
+	*/
+
 	int i;
 	cin >> i;
 	return 0;
