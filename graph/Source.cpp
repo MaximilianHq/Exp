a0eb252f915002515702f0c0844fa43473cabe32
Maximilian Hallqvist <98323670+NoLifer47@users.noreply.github.com>
2023-09-30 02:33:19 +0200

    added: availability to change graph range and the way the class handles range. delete function.
    changed:
    compiler version 14 -> 20
    
    OBS!
    faulty: deleteFunction()

--- graph/Source.cpp
+++ graph/Source.cpp
@@ -3,41 +3,39 @@
 #include <map>
 #include <string>
 #include <cmath>
-#include <functional>
-#include <algorithm>
 #include <format>
 
 using namespace std;
 
 namespace graph_color {
-	const string axis_x = "\033[31m";	// x-axis
-	const string axis_y = "\033[31m";	// y-axis
-	const string function = "\033[32m";	// graph
-	const string intersect = "\033[35m";// intersect (graph)
-	const string root = "\033[34m";		// x = 0
-	const string end = "\033[0m";		// end of color change
+	const string axis_x = "\033[31m";		// x-axis
+	const string axis_y = "\033[31m";		// y-axis
+	const string function = "\033[32m";		// graph
+	const string intersect = "\033[35m";	// intersect (graph)
+	const string root = "\033[34m";			// x = 0
+	const string end = "\033[0m";			// end of color change
 }
 
 class Graph {
 public:
 
 	Graph(int range) : range(range) {
-		// initialize the graph filling every vector with empty space
-		graph = vector<vector<string>>(rows, vector<string>(cols, " "));
+		setGraphRange(range);
 	}
 
 	void initializeGraph() {
+		graph = vector<vector<string>>(rows, vector<string>(cols, " "));
 		int range_x = 0;
 		int range_y = 0;
 
 		for (int i = 0; i < rows; i++)
 			for (int j = 0; j < cols; j++) {
 				// set numbers on x-axis
-				if (j == half_range) {
+				if (j == range_half) {
 					graph[i][j] = graph_color::axis_y + to_string(range_y) + graph_color::end;
 
 					// While on the positive side of 0
-					if (i < half_range)
+					if (i < range_half)
 						range_y--;
 					// While on the negative side of 0
 					else
@@ -50,15 +48,15 @@ public:
 						range_y = 0;
 
 					// move range_x once at center (replaced by y-axis)
-					if (i == half_range)
+					if (i == range_half)
 						range_x++;
 				}
 				// set numbers on y-axis
-				else if (i == half_range) {
+				else if (i == range_half) {
 					graph[i][j] = graph_color::axis_x + to_string(range_x) + graph_color::end;
 
 					// While on the negative side of 0
-					if (j < half_range)
+					if (j < range_half)
 						range_x--;
 					// While on the positive side of 0
 					else
@@ -113,10 +111,10 @@ public:
 				for (int j = 0; j < cols; j++)
 					for (auto& point : func)
 						// y becomes larger as i goes towards 0, while x becomes larger as i goes towards infinity
-						if (i == -point["y"] + half_range and j == point["x"] + half_range) {
-							if (i == half_range)
+						if (i == -point["y"] + range_half and j == point["x"] + range_half) {
+							if (i == range_half)
 								graph[i][j] = graph_color::root + "0" + graph_color::end;
-							else if (graph[i][j] != " " and j != half_range) // <-- prevent y axis override
+							else if (graph[i][j] != " " and j != range_half) // <-- prevent y axis override
 								graph[i][j] = graph_color::intersect + "0" + graph_color::end;
 							else
 								graph[i][j] = graph_color::function + "0" + graph_color::end;
@@ -128,33 +126,41 @@ public:
 		for (auto& func : functions) {
 			cout << "\nFunction " << func_index + 1 << endl;
 			cout << "f(x):";
-			for (auto& var : function_variables[func_index])
+			for (auto& var : function_variables[func_index++])
 				cout << " " << var;
 			cout << endl;
 		}
 	}
 
 	bool deleteFunction(int& i) {
-		if (i >= 0 && i < functions.size()) {
+		--i;
+		if (i >= 0 and i < functions.size()) {
 			// erase vector at specified index
 			functions.erase(functions.begin() + i);
 			return true;
 		}
 		else {
-			std::cout << "Index is out of bounds." << std::endl;
+			std::cout << format("Index ({}) is out of bounds.", ++i) << std::endl;
 			return false;
 		}
 	}
 
+	void setGraphRange(int& r) {
+		// needs safety
+		range = r;
+		rows = range + 1;
+		cols = range + 1;
+		range_half = range / 2;
+	}
+
 private:
 	int range;
-	int rows = range + 1;
-	int cols = range + 1;
-	int half_range = range / 2;
+	int rows;
+	int cols;
+	int range_half;
 
 	vector<vector<map<string, double>>> functions;
 	vector<vector<double>> function_variables;
-
 	vector<vector<string>> graph;
 };
 
@@ -162,9 +168,11 @@ void displayMenu() {
 	cout << "\033[1;36m"
 		"\n=============="
 		"\n1. New function"
-		"\n2. Edit / Delete"
-		"\n3. Plot Graph"
-		"\n4. Exit"
+		"\n2. Function list"
+		"\n3. Delete function"
+		"\n4. Plot Graph"
+		"\n5. Graph range"
+		"\n6. Exit"
 		"\n=============="
 		"\033[0m" << endl;
 }
@@ -172,10 +180,9 @@ void displayMenu() {
 void displayMenuEdit() {
 	cout << "\033[1;31m"
 		"\n=============="
-		"\n1. Edit"
-		"\n2. Delete"
-		"\n3. Reset"
-		"\n4. Exit"
+		"\n1. Delete"
+		"\n2. Reset"
+		"\n3. Exit"
 		"\n=============="
 		"\033[0m" << endl;
 }
@@ -186,45 +193,50 @@ void InputBracket(T& cinput) {
 	cin >> cinput;
 }
 
-void GraphEditLogic(Graph& calculator) {
+void MenuDeleteLogic(Graph& calculator) {
 	int input;
-	const int EXIT = 4;
+	const int EXIT = 3;
 
 	do {
+		input = EXIT;
 		calculator.displayFunctions();
 		displayMenuEdit();
-
 		InputBracket(input);
 
 		switch (input)
 		{
 		case 1: {
-		}
-		case 2: {
 			cout << "Select function to delete:" << endl;
 			do {
 				InputBracket(input);
+				cout << input;
 			} while (!calculator.deleteFunction(input));
+			break;
 		}
-		case 3: {
+		case 2: {
+			int i = 1;
+			while (calculator.deleteFunction(i)) {
+				i++;
+			}
+			break;
 		}
 		default:
+			cout << format("Invalid input ({}), out of range.", input) << endl;
 			break;
 		}
 	} while (input != EXIT);
 }
 
 int main() {
-	const int RANGE = 40;
+	int graph_range = 40;
+	Graph calculator(graph_range);
 
-	Graph calculator(RANGE);
-
-	const int MENU_EXIT = 4;
-	int menu_choice = MENU_EXIT;
+	const int MENU_EXIT = 6;
+	int menu_choice;
 
 	do {
+		menu_choice = MENU_EXIT;
 		displayMenu();
-
 		InputBracket(menu_choice);
 
 		switch (menu_choice)
@@ -234,9 +246,10 @@ int main() {
 			vector<double> function_var;
 
 			cout << "Enter variables, 's' to stop, ... ax ^ 2 + bx + c" << endl;
+			cout << ">>> ";
 
 			do {
-				InputBracket(input);
+				cin >> input;
 
 				if (cin.fail()) {
 					cin.clear();
@@ -252,18 +265,29 @@ int main() {
 			break;
 		}
 		case 2: {
-			GraphEditLogic(calculator);
-
+			calculator.displayFunctions();
 			break;
 		}
 		case 3: {
+			MenuDeleteLogic(calculator);
+			break;
+		}
+		case 4: {
 			calculator.initializeGraph();
 			calculator.plotFunctions();
 			calculator.plotGraph();
 
 			break;
 		}
+		case 5: {
+			int new_range;
+			InputBracket(new_range);
+			calculator.setGraphRange(new_range);
+
+			break;
+		}
 		default:
+			cout << format("Invalid input ({}), out of range.", menu_choice) << endl;
 			break;
 		}
 	} while (menu_choice != MENU_EXIT);
